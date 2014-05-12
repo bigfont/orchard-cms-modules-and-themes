dc8e601108276a0dc28952f26843794bd4664a57
Shaun Luttin <shaunluttin@gmail.com>
2014-05-12 16:44:22 -0700

    Add a ListShapeProvider and hit its breakpoint.

---
+++ src/Orchard.Web/Themes/Bootstrap.SimpleBlog/ListShapeProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bootstrap.SimpleBlog
+{
+    using Orchard.DisplayManagement.Descriptors;
+    public class ListShapeProvider : IShapeTableProvider
+    {
+        public void Discover(ShapeTableBuilder builder)
+        {
+            System.Diagnostics.Debugger.Break();
+            // implementation here
+        }
+    }
+}
