84d11275b9481fd4f125c962beb596fd592fc3e6
Shaun Luttin <shaunluttin@gmail.com>
2014-05-12 18:17:51 -0700

    Use Orchard helpers for the List View.

--- src/Orchard.Web/Themes/Bootstrap.SimpleBlog/ListShapeProvider.cs
+++ src/Orchard.Web/Themes/Bootstrap.SimpleBlog/ListShapeProvider.cs
@@ -10,8 +10,23 @@ namespace Bootstrap.SimpleBlog
     {
         public void Discover(ShapeTableBuilder builder)
         {
-            this.AddAlternateShapeOnFrontEnd(builder, "List", "List__Bootstrap__ListGroup");
-            this.AddAlternateShapeOnFrontEnd(builder, "Parts_Blogs_BlogPost_List", "List__Bootstrap__ListGroup");
+            builder.Describe("List").OnDisplaying(displaying =>
+            {
+                // make sure we are on the front end
+                if (displaying.ShapeMetadata.DisplayType == null || !displaying.ShapeMetadata.DisplayType.Contains("Admin"))
+                {
+                    displaying.ShapeMetadata.Alternates.Add("List__Bootstrap__ListGroup");
+                }
+            });
+
+            builder.Describe("Parts_Blogs_BlogPost_List").OnDisplaying(displaying =>
+            {
+                // make sure we are on the front end
+                if (displaying.ShapeMetadata.DisplayType == null || !displaying.ShapeMetadata.DisplayType.Contains("Admin"))
+                {
+                    displaying.ShapeMetadata.Alternates.Add("List__Bootstrap__ListGroup");
+                }
+            });
         }
 
         private void AddAlternateShapeOnFrontEnd(ShapeTableBuilder builder, string shapeName, string alternateName)
