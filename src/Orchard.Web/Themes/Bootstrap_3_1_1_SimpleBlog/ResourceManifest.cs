b206d1500d8543f6ab0a8d3e0334b995237495da
Shaun Luttin <shaunluttin@gmail.com>
2014-05-21 17:40:47 -0700

    Add js for apa style.

---
+++ src/Orchard.Web/Themes/Bootstrap_3_1_1_SimpleBlog/ResourceManifest.cs
@@ -0,0 +1,23 @@
+using Orchard.UI.Resources;
+
+namespace Bootstrap_3_1_1_SimpleBlog
+{
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+
+            var manifest = builder.Add();
+            manifest.DefineScript("html5shiv").SetCdn("//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js");
+            manifest.DefineScript("respondJS").SetCdn("//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js");
+            manifest.DefineScript("Bootstrap").SetCdn("//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js").SetDependencies("jQuery");
+
+
+
+            manifest.DefineScript("APA-Style").SetUrl("APA-Style.js").SetDependencies("jQuery");
+
+            manifest.DefineStyle("Bootstrap").SetCdn("//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css");
+            manifest.DefineStyle("FontAwesome").SetCdn("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css");
+            manifest.DefineStyle("APA-Style").SetUrl("APA-Style.css");
+            manifest.DefineStyle("Site").SetUrl("Site.css");
+        }
+    }
+}
---
+++ src/Orchard.Web/Themes/Bootstrap_3_1_1_SimpleBlog/Scripts/LiveWriter.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+
+    $('.blog-post').find('h3,h4,h5').each(function () {
+        $this = $(this);
+        $this.add($this.next('p')).wrapAll('<div class="run-in" />');
+    })
+
+});
+
