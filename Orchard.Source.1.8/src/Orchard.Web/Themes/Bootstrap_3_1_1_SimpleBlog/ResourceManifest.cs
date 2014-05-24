f2e3221b221c0b664d330a71a03ae682216b9a00
Shaun Luttin <shaunluttin@gmail.com>
2014-05-24 15:52:46 -0700

    Add ie-10 support to simple blog.

--- Orchard.Source.1.8/src/Orchard.Web/Themes/Bootstrap_3_1_1_SimpleBlog/ResourceManifest.cs
+++ Orchard.Source.1.8/src/Orchard.Web/Themes/Bootstrap_3_1_1_SimpleBlog/ResourceManifest.cs
@@ -16,7 +16,7 @@ namespace Bootstrap_3_1_1_SimpleBlog
             manifest.DefineStyle("Bootstrap").SetCdn("//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css");
             manifest.DefineStyle("FontAwesome").SetCdn("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css");
             manifest.DefineStyle("APA-Style").SetUrl("APA-Style.css");
-            manifest.DefineScript("IE-10").SetUrl("IE-10.css");
+            manifest.DefineStyle("IE-10").SetUrl("IE-10.css");
             manifest.DefineStyle("Site").SetUrl("Site.css");
         }
     }
