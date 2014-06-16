bedd220f1b90a4809617ac20488769083e187447
Shaun Luttin <shaun@bigfont.ca>
2014-06-16 15:14:20 -0400

    Refresh to Windows 8.1

--- Orchard.Source.1.8/src/Orchard.Web/Modules/Nwazet.ZenGallery/Tokens/ITokenProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Orchard.Events;
-using Orchard.Tokens;
-
-namespace Nwazet.ZenGallery.Tokens {
-    public interface ITokenProvider : IEventHandler {
-        void Describe(DescribeContext context);
-        void Evaluate(EvaluateContext context);
-    }
-}
