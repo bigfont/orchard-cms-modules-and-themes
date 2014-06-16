1983bbd4640173d8a0d2262bb5f5ed72a6162ced
Shaun Luttin <shaun@bigfont.ca>
2014-06-16 17:27:55 -0400

    Make picasa web albums for TSOKH.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/Picasa/Models/PicasaWidget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Records;
+using System.ComponentModel;
+
+namespace Picasa.Models
+{
+    public class PicasaWidgetRecord : ContentPartRecord
+    {
+        public virtual string Username { get; set; }
+    }
+
+    public class PicasaWidgetPart : ContentPart<PicasaWidgetRecord>
+    {
+        [Required]
+        [DisplayName("Picasa Username")]
+        public string Username
+        {
+            get { return Record.Username; }
+            set { Record.Username = value; }
+        }
+
+    }
+}
