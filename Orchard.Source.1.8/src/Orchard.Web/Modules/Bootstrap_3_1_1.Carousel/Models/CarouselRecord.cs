98615f0e3ef799dee48301d7f0adc46ff448b7d7
Shaun Luttin <shaun@bigfont.ca>
2014-06-17 19:22:23 -0400

    Update Bootstrap_3_1_1_KongwaHill.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/Bootstrap_3_1_1.Carousel/Models/CarouselRecord.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.ContentManagement.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bootstrap_3_1_1.Carousel.Models
+{
+    public class CarouselRecord : ContentPartRecord
+    {
+
+    }
+}
