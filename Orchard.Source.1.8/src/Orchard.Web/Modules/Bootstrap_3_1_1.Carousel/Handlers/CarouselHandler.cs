c568d7fcc6f13f092cfce1d9f6c48220f40b92ec
Shaun Luttin <shaun@bigfont.ca>
2014-06-17 19:22:23 -0400

    Update Bootstrap_3_1_1_KongwaHill.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/Bootstrap_3_1_1.Carousel/Handlers/CarouselHandler.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.ContentManagement.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bootstrap_3_1_1.Carousel.Handlers
+{
+    public class CarouselHandler : ContentHandler
+    {
+    }
+}
