ad38980b908ff2da558fb333fa544fdb6c5569de
Shaun Luttin <shaun@bigfont.ca>
2014-06-20 09:18:56 -0400

    Draft scaffolding of FingerPrintResourceManager.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.FingerPrint/FingerPrintResourceManager.cs
@@ -0,0 +1,20 @@
+﻿using Autofac.Features.Metadata;
+using Orchard.Environment.Extensions;
+using Orchard.UI.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.FingerPrint
+{
+    [OrchardSuppressDependency("Orchard.UI.Resources.ResourceManager")]
+    public class FingerPrintResourceManager : ResourceManager
+    {
+        public FingerPrintResourceManager(IEnumerable<Meta<IResourceManifestProvider>> resourceProviders)
+            : base(resourceProviders)
+        {
+            System.Diagnostics.Debugger.Break();
+        }
+    }
+}
