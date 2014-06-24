db9f6aed10d511abb1c82e1473d037e0bbc83039
Shaun Luttin <shaun@bigfont.ca>
2014-06-24 09:35:49 -0700

    Add two BigFont modules.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/_Backup/BigFont.ContactUs/Handlers/BigFontContactUsHandler.cs
@@ -0,0 +1,11 @@
+﻿using BigFont.ContactUs.Models;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+
+namespace BigFont.ContactUs.Handlers {
+    public class BigFontContactUsHandler : ContentHandler {
+        public BigFontContactUsHandler(IRepository<BigFontContactUsRecord> repository) {
+            Filters.Add(StorageFilter.For(repository));
+        }
+    }
+}
