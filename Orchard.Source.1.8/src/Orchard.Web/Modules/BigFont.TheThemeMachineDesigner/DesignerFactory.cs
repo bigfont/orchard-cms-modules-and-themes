d3b6792d267b21740422358bd9f8adb708fa8a22
Shaun Luttin <shaun@bigfont.ca>
2014-06-27 15:14:00 -0700

    Add module for TheThemeMachineDesigner.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/DesignerFactory.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.DisplayManagement.Implementation;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.TheThemeMachineDesigner
+{
+    [OrchardFeature("BigFont.TheThemeMachineDesigner")]
+    public class DesignerFactory : IShapeFactoryEvents, IShapeDisplayEvents
+    {
+        public void Creating(ShapeCreatingContext context)
+        {
+            int i = 0;
+            ++i;
+        }
+        public void Created(ShapeCreatedContext context)
+        {
+            int i = 0;
+            ++i;
+        }
+        public void Displaying(ShapeDisplayingContext context)
+        {
+            int i = 0;
+            ++i;
+        }
+        public void Displayed(ShapeDisplayedContext context)
+        {
+            int i = 0;
+            ++i;
+        }
+    }
+}
