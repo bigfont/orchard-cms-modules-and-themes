1b9c05bd737bc533560600b3e5354dc8125d63e7
Shaun Luttin <shaun@bigfont.ca>
2014-06-30 14:02:36 -0700

    Include a stylesheet in the head from a module.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Services/DesignerFactory.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.DisplayManagement.Implementation;
+using Orchard.DisplayManagement.Shapes;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.TheThemeMachineDesigner
+{
+    [OrchardFeature("BigFont.TheThemeMachineDesigner")]
+    public class DesignerFactory : IShapeFactoryEvents, IShapeDisplayEvents
+    {
+        public void Creating(ShapeCreatingContext context) { }
+        public void Created(ShapeCreatedContext context)
+        {
+            System.Diagnostics.Debug.Print(context.ShapeType);
+
+            if (context.ShapeType != "Layout"
+                && context.ShapeType != "DocumentZone"
+                && context.ShapeType != "PlaceChildContent"
+                && context.ShapeType != "ContentZone"
+                && context.ShapeType != "ShapeTracingMeta"
+                && context.ShapeType != "ShapeTracingTemplates"
+                && context.ShapeType != "DateTimeRelative")
+            {
+                var shapeMetadata = (ShapeMetadata)context.Shape.Metadata;
+                shapeMetadata.Wrappers.Add("ThemeMachineDesignerWrapper");
+            }
+        }
+        public void Displaying(ShapeDisplayingContext context) { }
+        public void Displayed(ShapeDisplayedContext context) { }
+    }
+}
