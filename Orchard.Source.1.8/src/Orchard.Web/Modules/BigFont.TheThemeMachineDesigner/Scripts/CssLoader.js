8f0129383e08a0b0c5bbf45693cfc8b931fb2966
Shaun Luttin <shaun@bigfont.ca>
2014-06-30 15:07:53 -0700

    Add TheThemeMachineDesigner css to a module.

---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/CssLoader.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    
+    var cssPath = "/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Styles/module.css";
+
+    $('head').append('<link rel="stylesheet" href=' + cssPath + ' type="text/css" />');
+
+    console.log('Appended to head: ' + cssPath);
+});
--- Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Services/DesignerFactory.cs
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Services/DesignerFactory.cs
@@ -1,20 +1,49 @@
-﻿using Orchard.DisplayManagement.Implementation;
+﻿using Orchard;
+using Orchard.DisplayManagement.Implementation;
 using Orchard.DisplayManagement.Shapes;
 using Orchard.Environment.Extensions;
+using Orchard.Security;
+using Orchard.UI.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 
 namespace BigFont.TheThemeMachineDesigner
 {
     [OrchardFeature("BigFont.TheThemeMachineDesigner")]
     public class DesignerFactory : IShapeFactoryEvents, IShapeDisplayEvents
     {
+        private readonly WorkContext _workContext;
+        private readonly IAuthorizer _authorizer;
+
+        private bool IsActivable()
+        {
+            // activate on front-end only
+            if (AdminFilter.IsApplied(new RequestContext(_workContext.HttpContext, new RouteData())))
+                return false;
+
+            // if not logged as a site owner, still activate if it's a local request (development machine)
+            if (!_authorizer.Authorize(StandardPermissions.SiteOwner))
+                return _workContext.HttpContext.Request.IsLocal;
+
+            return true;
+        }
+
+        public DesignerFactory(IWorkContextAccessor workContextAccessor, IAuthorizer authorizer)
+        {
+            _workContext = workContextAccessor.GetContext();
+            _authorizer = authorizer;
+        }
+
         public void Creating(ShapeCreatingContext context) { }
         public void Created(ShapeCreatedContext context)
         {
-            System.Diagnostics.Debug.Print(context.ShapeType);
+            if (!IsActivable())
+            {
+                return;
+            }            
 
             if (context.ShapeType != "Layout"
                 && context.ShapeType != "DocumentZone"
