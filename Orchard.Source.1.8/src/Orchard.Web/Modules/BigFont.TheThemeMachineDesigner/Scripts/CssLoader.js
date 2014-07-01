a9b9003c0d076f2cd4980669b21891ff48a95ccd
Shaun Luttin <shaun@bigfont.ca>
2014-06-30 20:01:49 -0700

    Get the base url.

--- Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/CssLoader.js
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/CssLoader.js
@@ -1,7 +1,35 @@
-﻿$(function () {    
-    var cssPath = "/Modules/BigFont.TheThemeMachineDesigner/Styles/module.css";
+﻿(function () {
 
-    $('head').append('<link rel="stylesheet" href=' + cssPath + ' type="text/css" />');
+    // see http://www.gotknowhow.com/articles/how-to-get-the-base-url-with-javascript
+    function getBaseURL() {
+        var url = location.href;  // entire url including querystring - also: window.location.href;
+        var baseURL = url.substring(0, url.indexOf('/', 14));
 
-    console.log('Appended to head: ' + cssPath);
-});
+
+        if (baseURL.indexOf('http://localhost') != -1) {
+            // Base Url for localhost
+            var url = location.href;  // window.location.href;
+            var pathname = location.pathname;  // window.location.pathname;
+            var index1 = url.indexOf(pathname);
+            var index2 = url.indexOf("/", index1 + 1);
+            var baseLocalUrl = url.substr(0, index2);
+
+            return baseLocalUrl + "/";
+        }
+        else {
+            // Root Url for domain name
+            return baseURL + "/";
+        }
+
+    }
+
+    $(function () {
+        var cssPath = getBaseURL() + "/Modules/BigFont.TheThemeMachineDesigner/Styles/module.css";
+
+        $('head').append('<link rel="stylesheet" href=' + cssPath + ' type="text/css" />');
+
+        console.log('Appended to head: ' + cssPath);
+    });
+
+}(location));
+
---
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/_Backup/BigFont.TheThemeMachineDesigner/Scripts/CssLoader.js
@@ -0,0 +1,7 @@
+﻿$(function () {    
+    var cssPath = "/Modules/BigFont.TheThemeMachineDesigner/Styles/module.css";
+
+    $('head').append('<link rel="stylesheet" href=' + cssPath + ' type="text/css" />');
+
+    console.log('Appended to head: ' + cssPath);
+});
