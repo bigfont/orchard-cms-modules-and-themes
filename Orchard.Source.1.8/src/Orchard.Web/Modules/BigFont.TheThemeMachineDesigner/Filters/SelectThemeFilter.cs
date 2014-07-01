28f06a5d9cc058b52302b3cc04db70f84c4edb85
Shaun Luttin <shaun@bigfont.ca>
2014-07-01 10:40:16 -0700

    Add rudimentry theme selector.

--- Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Filters/SelectThemeFilter.cs
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Filters/SelectThemeFilter.cs
@@ -1,32 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+using Orchard;
 using Orchard.Mvc.Filters;
 using Orchard.Environment.Features;
 using Orchard.Environment.Extensions.Models;
-using Orchard;
 using Orchard.DisplayManagement;
+using Orchard.Themes.Services;
+using Orchard.Themes.Models;
 
 namespace BigFont.TheThemeMachineDesigner.Filters
 {
     public class SelectThemeFilter : FilterProvider, IResultFilter
     {
         private readonly IFeatureManager _featureManager;
-        private readonly IWorkContextAccessor _workContextAccessor; 
+        private readonly IWorkContextAccessor _workContextAccessor;
         private readonly dynamic _shapeFactory;
+        private ISiteThemeService _siteThemeService;
 
 
-        public SelectThemeFilter(IFeatureManager featureManager, IWorkContextAccessor workContextAccessor, IShapeFactory shapeFactory)
+        public SelectThemeFilter(
+            IFeatureManager featureManager, 
+            IWorkContextAccessor workContextAccessor, 
+            IShapeFactory shapeFactory, 
+            ISiteThemeService siteThemeService)
         {
             _featureManager = featureManager;
             _workContextAccessor = workContextAccessor;
             _shapeFactory = shapeFactory;
+            _siteThemeService = siteThemeService;
         }
 
         public void OnResultExecuting(ResultExecutingContext filterContext)
         {
+            var currentTheme = GetCurrentThemeEntry();
+
             var installedThemes = _featureManager.GetEnabledFeatures()
                 .Select(x => x.Extension)
                 .Where(x => DefaultExtensionTypes.IsTheme(x.ExtensionType))
@@ -36,7 +46,8 @@ namespace BigFont.TheThemeMachineDesigner.Filters
                 .Select(theme => new SelectListItem
                 {
                     Text = theme.Name,
-                    Value = theme.Id
+                    Value = theme.Id,
+                    Selected = theme.Name == currentTheme.Name
                 })
                 .ToList();
 
@@ -44,5 +55,16 @@ namespace BigFont.TheThemeMachineDesigner.Filters
         }
 
         public void OnResultExecuted(ResultExecutedContext filterContext) { }
+
+        private ThemeEntry GetCurrentThemeEntry()
+        {
+            ThemeEntry currentTheme = null;
+            ExtensionDescriptor currentThemeDescriptor = _siteThemeService.GetSiteTheme();
+            if (currentThemeDescriptor != null)
+            {
+                currentTheme = new ThemeEntry(currentThemeDescriptor);
+            }
+            return currentTheme;
+        }
     }
 }
