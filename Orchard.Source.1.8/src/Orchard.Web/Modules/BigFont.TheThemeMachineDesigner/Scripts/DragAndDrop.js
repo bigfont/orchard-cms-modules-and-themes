6bc5449e4d42ca94ae2700c3825d126b2ace0ea7
Shaun Luttin <shaun@bigfont.ca>
2014-07-07 12:35:31 -0700

    Finished and linted.

--- Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/DragAndDrop.js
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/DragAndDrop.js
@@ -1,33 +1,34 @@
-﻿function drag_start(event) {
+﻿/*global $:false, window: false, document: false */
 
-    console.log("drag_start");
+(function (window, document) {
 
-    var style = window.getComputedStyle(event.target, null);
-    event
-        .dataTransfer
-        .setData(
-            "text/plain",
-            (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' +
-            (parseInt(style.getPropertyValue("top"), 10) - event.clientY));
-}
-function drag_over(event) {
-    event.preventDefault();
-    return false;
-}
-function drop(event) {
-    var offset = event.dataTransfer.getData("text/plain").split(',');
-    var dm = document.getElementById('theme-designer-control-bar');
-    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-    dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
-    event.preventDefault();
-    return false;
-}
+    "use strict";
 
-$(function () {
+    function drag_start(event) {
+        var style = window.getComputedStyle(event.target, null);
+        event
+            .dataTransfer
+            .setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY));
+    }
+    function drag_over(event) {
+        event.preventDefault();
+        return false;
+    }
+    function drop(event) {
+        var offset, dm;
 
-    var dm = document.getElementById('theme-designer-control-bar');
-    dm.addEventListener('dragstart', drag_start, false);
-    document.body.addEventListener('dragover', drag_over, false);
-    document.body.addEventListener('drop', drop, false);
+        offset = event.dataTransfer.getData("text/plain").split(',');
+        dm = document.getElementById('theme-designer-control-bar');
+        dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+        dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+        event.preventDefault();
+        return false;
+    }
 
-});
+    $(function () {
+        var dm = document.getElementById('theme-designer-control-bar');
+        dm.addEventListener('dragstart', drag_start, false);
+        document.body.addEventListener('dragover', drag_over, false);
+        document.body.addEventListener('drop', drop, false);
+    });
+}(window, document));
