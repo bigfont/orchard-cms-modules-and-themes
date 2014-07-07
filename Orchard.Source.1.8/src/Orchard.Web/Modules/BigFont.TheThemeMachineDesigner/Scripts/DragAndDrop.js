0b41a235e864748f9afec0cebcecff548b88cf3e
Shaun Luttin <shaun@bigfont.ca>
2014-07-07 13:26:32 -0700

    Refactor.

--- Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/DragAndDrop.js
+++ Orchard.Source.1.8/src/Orchard.Web/Modules/BigFont.TheThemeMachineDesigner/Scripts/DragAndDrop.js
@@ -4,29 +4,43 @@
 
     "use strict";
 
+    var draggedElementId = 'theme-designer-control-bar';
+
     function drag_start(event) {
-        var style = window.getComputedStyle(event.target, null);
-        event
-            .dataTransfer
-            .setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY));
+        var style, top, left, startPoint;
+        style = window.getComputedStyle(event.target, null);
+
+        // get the original position
+        top = (parseInt(style.getPropertyValue("top"), 10) - event.clientY);
+        left = (parseInt(style.getPropertyValue("left"), 10) - event.clientX);
+        startPoint = left + ',' + top;
+
+        // send it to the drop event
+        event.dataTransfer.setData("text/plain", startPoint);
     }
     function drag_over(event) {
         event.preventDefault();
         return false;
     }
     function drop(event) {
-        var offset, dm;
+        var startPoint, dm, top, left;
 
-        offset = event.dataTransfer.getData("text/plain").split(',');
-        dm = document.getElementById('theme-designer-control-bar');
-        dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-        dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+        startPoint = event.dataTransfer.getData("text/plain").split(',');
+        dm = document.getElementById(draggedElementId);
+
+        // calculate and set the new position
+        left = (event.clientX + parseInt(startPoint[0], 10));
+        top = (event.clientY + parseInt(startPoint[1], 10));
+        dm.style.left = left + 'px';
+        dm.style.top = top + 'px';
+
+        // prevent default
         event.preventDefault();
         return false;
     }
 
     $(function () {
-        var dm = document.getElementById('theme-designer-control-bar');
+        var dm = document.getElementById(draggedElementId);
         dm.addEventListener('dragstart', drag_start, false);
         document.body.addEventListener('dragover', drag_over, false);
         document.body.addEventListener('drop', drop, false);
