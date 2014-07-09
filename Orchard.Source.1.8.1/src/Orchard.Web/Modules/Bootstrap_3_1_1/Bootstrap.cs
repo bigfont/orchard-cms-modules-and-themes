f1ddc34fa125f41b9531fdd1d25b397b247f6100
Shaun Luttin <shaun@bigfont.ca>
2014-07-09 12:09:17 -0700

    Add a Bootstrap extension class.

---
+++ Orchard.Source.1.8.1/src/Orchard.Web/Modules/Bootstrap_3_1_1/Bootstrap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Bootstrap
+{ 
+    // note: html css classes are case sensitive
+    public enum Class
+    {
+        thumbnail
+    }
+
+    public static string Column(int xs, int sm, int md, int lg)
+    {
+        return string.Format("col-xs-{0} col-sm{1} col-md-{2} col-lg-{3}", xs, sm, md, lg);    
+    }
+}
