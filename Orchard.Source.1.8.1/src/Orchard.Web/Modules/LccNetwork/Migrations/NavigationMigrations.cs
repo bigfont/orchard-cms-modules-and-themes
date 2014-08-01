21e75dce74ff4d22f87c60bc3c876b7ec29ed70e
Shaun Luttin <shaun@bigfont.ca>
2014-08-01 02:08:03 -0700

    Add section menu.

---
+++ Orchard.Source.1.8.1/src/Orchard.Web/Modules/LccNetwork/Migrations/NavigationMigrations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+using Orchard.Environment.Extensions;
+
+namespace LccNetwork.Migrations
+{
+    [OrchardFeature("SectionNavigation")]
+    public class NavigationMigrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            return 2;
+        }
+
+        public int UpdateFrom2()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("SectionMenu",
+                    cfg => cfg
+                    .WithPart("CommonPart")
+                    .WithPart("IdentityPart")
+                    .WithPart("WidgetPart")
+                    .WithPart("MenuWidgetPart") // why cannot we add MenuWidgetPart through the UI?
+                    .WithSetting("Stereotype", "Widget")
+                );
+
+            return 3;
+        }
+    }
+}
