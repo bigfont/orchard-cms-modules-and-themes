f61999f4863ae5b396a13dbceb7a412342051b89
Shaun Luttin <shaun@bigfont.ca>
2014-08-26 10:54:11 -0700

    Add the Lcc Map.

---
+++ Orchard.Source.1.8.1/src/Orchard.Web/Modules/LccNetwork/Controllers/StaticPages.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.Environment.Extensions;
+using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LccNetwork.Controllers
+{
+    [Themed]
+    [OrchardFeature("StaticPages")]
+    public class StaticPages : Controller
+    {
+        public ViewResult FindAnLcc()
+        {
+            return View();
+        }
+    }
+}
---
+++ Orchard.Source.1.8.1/src/Orchard.Web/Modules/LccNetwork/Routes.cs
@@ -0,0 +1,48 @@
+﻿using Orchard.Environment.Extensions;
+using Orchard.Mvc.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LccNetwork
+{
+    [OrchardFeature("FindAnLcc")]
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] { 
+                new RouteDescriptor {
+                    Priority = 5, 
+                    Route = new Route(
+                        // url
+                        "find-an-lcc",  
+                        // defaults
+                        new RouteValueDictionary { 
+                            { "area", "LccNetwork" }, 
+                            { "controller", "StaticPages" }, 
+                            { "action", "FindAnLcc" } },
+                        // constraints
+                        new RouteValueDictionary(),     
+                        // data tokens
+                        new RouteValueDictionary {
+                            {"area", "LccNetwork" }, 
+                        },     
+                        // route handler
+                        new MvcRouteHandler())          
+                }
+            };
+        }
+    }
+}
