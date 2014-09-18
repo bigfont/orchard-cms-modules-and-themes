f4be153346f7fc75e6c7905489e8d5478a475ad1
Shaun Luttin <shaunluttin@gmail.com>
2014-09-18 09:33:54 -0700

    Build bootstrap less.

--- Orchard.Source.1.8.1/src/Orchard.Web/Themes/Bootstrap_3_2_0_Base/ResourceManifest.cs
+++ Orchard.Source.1.8.1/src/Orchard.Web/Themes/Bootstrap_3_2_0_Base/ResourceManifest.cs
@@ -4,8 +4,8 @@ namespace Bootstrap_3_1_1_Base {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            manifest.DefineScript("Bootstrap").SetUrl("Bootstrap/bootstrap.min.js").SetVersion("3.1.1");
-            manifest.DefineStyle("Bootstrap").SetUrl("Bootstrap/bootstrap.min.css").SetVersion("3.1.1");
+            manifest.DefineScript("Bootstrap").SetUrl("Bootstrap/bootstrap.min.js").SetVersion("3.2.0");
+            manifest.DefineStyle("Bootstrap").SetUrl("Bootstrap/less/bootstrap.min.css").SetVersion("3.2.0");
         }
     }
 }
