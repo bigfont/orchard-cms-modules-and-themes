fd9a654883e5241c8a5853fcdc5dfd331fbcde37
Shaun Luttin <shaunluttin@gmail.com>
2014-09-18 10:49:31 -0700

    Add bootstrap dist.

--- Orchard.Source.1.8.1/src/Orchard.Web/Themes/Bootstrap_3_2_0_Base/ResourceManifest.cs
+++ Orchard.Source.1.8.1/src/Orchard.Web/Themes/Bootstrap_3_2_0_Base/ResourceManifest.cs
@@ -4,7 +4,7 @@ namespace Bootstrap_3_1_1_Base {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-            manifest.DefineScript("Bootstrap").SetUrl("Bootstrap/bootstrap.min.js").SetVersion("3.2.0");
+            manifest.DefineScript("Bootstrap").SetUrl("bootstrap.min.js").SetVersion("3.2.0").SetDependencies("jQuery");
             manifest.DefineStyle("Bootstrap").SetUrl("Bootstrap/less/bootstrap.min.css").SetVersion("3.2.0");
         }
     }
