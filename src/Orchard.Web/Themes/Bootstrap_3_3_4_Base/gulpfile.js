01ea0b6709034565ac0221fbfda3eed5ec5c5d4f
Shaun Luttin <shaunluttin@gmail.com>
2015-05-25 19:08:14 -0700

    Build javascript.

--- src/Orchard.Web/Themes/Bootstrap_3_3_4_Base/gulpfile.js
+++ src/Orchard.Web/Themes/Bootstrap_3_3_4_Base/gulpfile.js
@@ -1,16 +1,18 @@
-var gulp = require('gulp')
-    less = require('gulp-less');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
 
 var paths = {
 
     bower : './bower_components/',
-    styles : './styles/',
-    scripts : '/scripts/',
+    styles : './Styles/',
+    scripts : './Scripts/',
     app : './app_components/'
 
 };
 
-gulp.task('default', function() {
+gulp.task('default', ['styles','scripts'], function() {
 
     
 
@@ -18,8 +20,25 @@ gulp.task('default', function() {
 
 gulp.task('styles', function () {
 
-    var stream = gulp.src(paths.app + '/app.less')
+    var stream = gulp.src(paths.app + '/base.less')
         .pipe(less())
         .pipe(gulp.dest(paths.styles));
+
+    return stream;
+
+});
+
+gulp.task('scripts', function () {
+
+    var stream = gulp.src(
+        [
+            paths.bower + 'jquery/dist/jquery.js',
+            paths.bower + 'bootstrap/dist/js/bootstrap.js'
+        ])
+        .pipe(concat('base.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.scripts));
+
+    return stream;
 
 });
